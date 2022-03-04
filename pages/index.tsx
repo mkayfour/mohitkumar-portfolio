694781c86124a4402781d6e621669629243a4c30
Mohit Kumar <mk4227525@gmail.com>
2022-03-04 22:23:35 +0530

    lets see if SEO works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+
 import Image from "next/image";
 
 import { SocialIcon } from "react-social-icons";
@@ -6,9 +8,47 @@ import { SocialIcon } from "react-social-icons";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const MY_SEO = {
+    title: "Mohit Kumar Srivastava",
+    description: "Full Stack Developer working with React JS and Node JS",
+    openGraph: {
+      type: "website",
+      url: "https://www.mkayfour.in/",
+      title: "Mohit Kumar Srivastava",
+      description: "Full Stack Developer working with React JS and Node JS",
+      image: "images/main.png",
+    },
+  };
+
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
+        <Head>
+          <title key="title">{MY_SEO.title}</title>
+          <meta
+            key="description"
+            name="description"
+            content={MY_SEO.description}
+          />
+          <meta key="og:type" name="og:type" content={MY_SEO.openGraph.type} />
+          <meta
+            key="og:title"
+            name="og:title"
+            content={MY_SEO.openGraph.title}
+          />
+          <meta
+            key="og:description"
+            name="og:description"
+            content={MY_SEO.openGraph.description}
+          />
+          <meta key="og:url" name="og:url" content={MY_SEO.openGraph.url} />
+          <meta
+            key="og:image"
+            name="og:image"
+            content={MY_SEO.openGraph.image}
+          />
+        </Head>
+
         <h1>Mohit Kumar Srivastava</h1>
         <Image
           src="/images/main.png"
